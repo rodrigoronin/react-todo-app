abd7e7668ffb0e478022217d3563681a7a3c21f2
rodrigoronin <liramailbox@gmail.com>
2025-11-27 11:32:42 -0300

    feat: new TaskItem component
    added a new component to isolate task items

--- src/components/TasksList/TasksList.tsx
+++ src/components/TasksList/TasksList.tsx
@@ -1,49 +1,47 @@
+import { useState } from "react";
 import AddTask from "../AddTask/AddTask";
 import Card from "../Card/Card";
 import Checkbox from "../Checkbox/Checkbox";
+import TaskItem from "../TaskItem/TaskItem";
 
+import type { Task } from "../../types/task";
 import style from "./TasksList.module.css";
 
-interface Task {
-  title: string;
-  completed: boolean;
-}
-
-const tasks: Task[] = [
-  {
-    title: "Daily shadow boxing training",
-    completed: false,
-  },
-  {
-    title: "Work on TODO app",
-    completed: true,
-  },
-  {
-    title: "Work on my game",
-    completed: false,
-  },
-  {
-    title: "Make coffee",
-    completed: true,
-  },
-  {
-    title: "Play some games",
-    completed: false,
-  },
-];
-
 const TaskList = () => {
+  const [tasks, setTasks] = useState<Task[]>([
+    {
+      id: "1",
+      title: "Daily shadow boxing training",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Work on TODO app",
+      completed: true,
+    },
+    {
+      id: "3",
+      title: "Work on my game",
+      completed: false,
+    },
+    {
+      id: "4",
+      title: "Make coffee",
+      completed: true,
+    },
+    {
+      id: "5",
+      title: "Play some games",
+      completed: false,
+    },
+  ]);
+
   return (
     <div className={style.container}>
       <AddTask />
       <div>
         {tasks.map((task, i) => (
-          <Card key={`${task.title}-${i}`}>
-            <Checkbox completed={task.completed} />
-            <span className={`${style.title} ${task.completed && style.completed} text-preset-1`}>
-              {task.title}
-            </span>
-          </Card>
+          <TaskItem key={`${task.title}-${i}`} task={task} />
         ))}
       </div>
     </div>
